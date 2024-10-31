0949476fd1f6d497569e830cb401d4d0bc5e9eef
Matei Simtinică <matei.simtinica@gmail.com>
2024-10-31 19:25:22 +0200

    updated tutorial 02 to recompute based on last time dataset is modified

--- NewTutorials/02 - Syftbox and DP/main.py
+++ NewTutorials/02 - Syftbox and DP/main.py
@@ -1,7 +1,9 @@
 from syftbox.lib import Client
+import os
 import sys
 import json
 import diffprivlib.tools as dp
+from pathlib import Path
 
 
 def compute_value(dataset):
@@ -12,22 +14,44 @@ def compute_value(dataset):
     )
 
 
+def check_dataset_modified(dataset_path: Path):
+    checkpoint_path = dataset_path.with_suffix(".timestamp")
+    last_modified = os.stat(dataset_path).st_mtime
+
+    # if there is no checkpoint file,
+    # consider the dataset changed, since this is the first computation
+    if not checkpoint_path.is_file():
+        with open(checkpoint_path, "w") as f:
+            f.write(str(last_modified))
+        return True
+
+    with open(checkpoint_path) as f:
+        checkpoint = float(f.read())
+
+    if last_modified == checkpoint:
+        return False
+
+    with open(checkpoint_path, "w") as f:
+        f.write(str(last_modified))
+
+    return True
+
+
 if __name__ == "__main__":
     client = Client.load()
 
     dataset_path = client.datasite_path / "datasets" / "dataset.json"
     value_path = client.datasite_path / "public" / "value.txt"
 
-    if value_path.exists():
-        print("\n========== Compute ==========\n")
-        print("value.txt file already exists. skipping execution...")
-        print(f"to force a re-execution, delete the value.txt file at {value_path}")
-        print("\n=============================\n")
-        sys.exit(0)
-
     if not dataset_path.exists() or not dataset_path.is_file():
         print("\n========== Compute ==========\n")
         print(f"dataset not found at {dataset_path}. skipping computation...")
+        print("\n=============================\n")
+        sys.exit(0)
+
+    if not check_dataset_modified(dataset_path):
+        print("\n========== Compute ==========\n")
+        print(f"dataset didn't change. skipping computation...")
         print("\n=============================\n")
         sys.exit(0)
 
