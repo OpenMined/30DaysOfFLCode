60e91dda3bdf492848ed967b920a091c35c9e3c4
Matei Simtinică <matei.simtinica@gmail.com>
2024-11-16 00:55:49 +0200

    move components to components directory

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -4,14 +4,16 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Heading from "@theme/Heading";
-import Timer from './timer/Timer';
-import Counter from './counter/Counter';
+import Timer from "../components/timer/Timer";
+import Counter from "../components/counter/Counter";
 
 import styles from "./index.module.css";
 
 // Deadline and duration constants
 const millisecondsPerDay = 24 * 60 * 60 * 1000;
-const daysUntilDeadline = Math.ceil((new Date('2024-12-20').getTime() - Date.now()) / millisecondsPerDay);
+const daysUntilDeadline = Math.ceil(
+  (new Date("2024-11-20").getTime() - Date.now()) / millisecondsPerDay,
+);
 const deadline = new Date(Date.now() + daysUntilDeadline * millisecondsPerDay);
 const duration = 30; // days
 const isDeadline = Date.now() > deadline.getTime();
@@ -20,14 +22,14 @@ startDate.setDate(startDate.getDate() - duration);
 
 function HeropageHeader() {
   const { siteConfig } = useDocusaurusContext();
-  
+
   return (
     <section className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <Heading as="h1" className="hero__title m-0 pt-8 pb-8">
+        <Heading as="h1" className="hero__title m-0 pb-8 pt-8">
           {siteConfig.title}
         </Heading>
-        <div className={`${styles.buttons} pt-8 pb-8`}>
+        <div className={`${styles.buttons} pb-8 pt-8`}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro"
@@ -35,7 +37,9 @@ function HeropageHeader() {
             Quickstart Guide - 5min ⏱️
           </Link>
         </div>
-        <p className="hero__subtitle text-black m-0 pt-8 pb-8">{siteConfig.tagline}</p>
+        <p className="hero__subtitle m-0 pb-8 pt-8 text-black">
+          {siteConfig.tagline}
+        </p>
         <Counter startDate={startDate} duration={duration} />
       </div>
     </section>
@@ -49,16 +53,16 @@ export default function Home(): JSX.Element {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />"
     >
-    {!isDeadline && (
-      <Timer
-        deadline={deadline}
-        onFinish={() => {
-          console.log('Countdown finished!');
-          // Add your desired actions here
-          alert('Countdown complete!');
-        }}
-      />
-    )}
+      {!isDeadline && (
+        <Timer
+          deadline={deadline}
+          onFinish={() => {
+            console.log("Countdown finished!");
+            // Add your desired actions here
+            alert("Countdown complete!");
+          }}
+        />
+      )}
       <HeropageHeader />
       <main>
         <HomepageFeatures />
